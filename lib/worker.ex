fedda9062853cc152914a6cba8d188e513002144
bkzhang <brian.k.zhang@gmail.com>
2017-05-03 13:22:48 -0400

    gets routes

--- lib/worker.ex
+++ lib/worker.ex
@@ -2,22 +2,28 @@ defmodule ElixirOC.Worker do
   @moduledoc """
   Worker that does all the requests.
   """
-  
+
   @doc """
   Returns route summary of the requested bus stop.
 
   ## Examples
 
-      iex> ElixirOC.Worker.route_summary(7659)
+      iex> ElixirOC.Worker.route_summary(7659, 1)
+      "Ottawa-Rockcliffe" 
 
   """
+  def route_summary(bus_stop, route_no) do
+    route_summary(bus_stop)
+    |> Map.get(route_no)
+  end
+  
   def route_summary(bus_stop) do
     result = route_url(bus_stop)
              |> :httpc.request
-             |> parse_res
+             |> parse_resp
     case result do
-      {:ok, json} ->
-        json
+      {:ok, routes} ->
+        Enum.reduce(routes, %{}, fn route, acc -> Map.put(acc, route["RouteNo"], route["RouteHeading"]) end) 
       :error ->
         "#{bus_stop} not found"
     end
@@ -27,7 +33,7 @@ defmodule ElixirOC.Worker do
     'https://api.octranspo1.com/v1.2/GetRouteSummaryForStop?appID=#{appID()}&apiKey=#{apikey()}&stopNo=#{bus_stop}&format=json'
   end
 
-  defp parse_res({:ok, {status, _, body}}) do 
+  defp parse_resp({:ok, {status, _, body}}) do 
     { _, status_code, _ } = status
     case status_code do
       200 ->
@@ -39,13 +45,14 @@ defmodule ElixirOC.Worker do
     end
   end
 
-  defp parse_res(_) do
+  defp parse_resp(_) do
     :error
   end
 
   defp fetch_route(json) do
     try do
-      {:ok, json}
+      routes = json["GetRouteSummaryForStopResult"]["Routes"]["Route"]
+      {:ok, routes}
     rescue
       _ ->
         :error
