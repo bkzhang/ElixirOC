0734c9e4f4b146c2abd59c08b67106d1fc65ac98
bkzhang <brian.k.zhang@gmail.com>
2017-05-06 01:59:16 -0400

    refactor

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -8,17 +8,15 @@ defmodule Benchmark do
   """
   @spec run(integer) :: atom
   def run(number) do 
-    IO.puts "Making #{number} requests through 200 processes"
     list = Stream.cycle([3060])
            |> Enum.take(number)
-    multi = time(fn -> ElixirOC.bus_routes_list(list) end) 
+    IO.puts "Making #{number} requests through 200 processes"
+    multi = 
+      time(fn -> ElixirOC.bus_routes_list(list) end) 
     
     IO.puts "Making #{number} requests through 1 process"
-    single = time(fn -> Enum.each(1..number, fn _ -> 
-                          ElixirOC.Worker.route_summary(3060) 
-                          |> IO.inspect
-                        end)
-                  end) 
+    single = 
+      time(fn -> Enum.each(1..number, fn _ -> ElixirOC.Worker.route_summary(3060) |> IO.inspect end) end)
 
     IO.puts "Multiple processes time: #{multi}"
     IO.puts "Single process time:     #{single}"
